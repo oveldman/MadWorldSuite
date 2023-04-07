948b2b21ded130185847650432d32bc1d00f68eb
Oscar Veldman <oveldman@gmail.com>
2023-04-07 12:57:08 +0200

    Add test functions

---
+++ MadWorld/MadWorld.Backend.API.Authorized/Functions/Test/Ping.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace MadWorld.Backend.API.Authorized.Functions.Test;
+
+public static class Ping
+{
+    [Function("Ping")]
+    public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        FunctionContext executionContext)
+    {
+        var logger = executionContext.GetLogger("Ping");
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        response.WriteString("Welcome to Azure Functions!");
+
+        return response;
+        
+    }
+}
