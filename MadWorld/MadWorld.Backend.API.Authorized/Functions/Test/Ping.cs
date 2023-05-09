7742586326a322ea25ec3b93ad03e1b0079d6511
Oscar Veldman <oveldman@gmail.com>
2023-05-09 17:26:02 +0200

    Remove name from ping

--- MadWorld/MadWorld.Backend.API.Authorized/Functions/Test/Ping.cs
+++ MadWorld/MadWorld.Backend.API.Authorized/Functions/Test/Ping.cs
@@ -17,19 +17,9 @@ public static class Ping
     [Function("Ping")]
     [OpenApiOperation(operationId: "Ping", tags: new[] { "Test" })]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-    public static HttpResponseData Run([Microsoft.Azure.Functions.Worker.HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+    public static string Run([Microsoft.Azure.Functions.Worker.HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
         FunctionContext executionContext)
     {
-        var user = executionContext.GetUser();
-        
-        var logger = executionContext.GetLogger("Ping");
-        logger.LogInformation("C# HTTP trigger function processed a request.");
-
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-        
-        response.WriteString($"Authorized: Welcome to Azure Functions, {user.Name}!");
-
-        return response;
+        return "Authorized: Welcome to Azure Functions!";
     }
 }
