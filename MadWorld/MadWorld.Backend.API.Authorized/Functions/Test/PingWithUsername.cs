563919513768f4e6fc1e0ee46d01bec7df8e3073
Oscar Veldman <oveldman@gmail.com>
2023-05-21 11:59:11 +0200

    Fix 401

--- MadWorld/MadWorld.Backend.API.Authorized/Functions/Test/PingWithUsername.cs
+++ MadWorld/MadWorld.Backend.API.Authorized/Functions/Test/PingWithUsername.cs
@@ -16,7 +16,7 @@ public static class PingWithUsername
     [OpenApiSecurity("Bearer", SecuritySchemeType.ApiKey, Name = "authorization", In = OpenApiSecurityLocationType.Header)]
     [OpenApiOperation(operationId: "PingWithUsername", tags: new[] { "Test" })]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-    public static string Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req,
+    public static string Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req,
         FunctionContext executionContext)
     {
         var user = executionContext.GetUser();
