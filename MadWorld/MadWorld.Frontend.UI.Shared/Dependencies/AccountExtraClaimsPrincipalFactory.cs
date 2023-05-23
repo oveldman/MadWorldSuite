627bb1517b15325f7a3bd375201489a5e60f352a
Oscar Veldman <oveldman@gmail.com>
2023-05-23 19:54:08 +0200

    Start account overview

--- MadWorld/MadWorld.Frontend.UI.Shared/Dependencies/AccountExtraClaimsPrincipalFactory.cs
+++ MadWorld/MadWorld.Frontend.UI.Shared/Dependencies/AccountExtraClaimsPrincipalFactory.cs
@@ -11,22 +11,29 @@ public class AccountExtraClaimsPrincipalFactory : AccountClaimsPrincipalFactory<
     {
     }
 
-    public override async ValueTask<ClaimsPrincipal> CreateUserAsync(RemoteUserAccount account, RemoteAuthenticationUserOptions options)
+    public override async ValueTask<ClaimsPrincipal> CreateUserAsync(RemoteUserAccount account,
+        RemoteAuthenticationUserOptions options)
     {
         var user = await base.CreateUserAsync(account, options);
 
-        if (user?.Identity?.IsAuthenticated ?? false)
-        {
-            var claimsIdentity = (ClaimsIdentity)user.Identity;
-            var roles = claimsIdentity
-                .Claims.FirstOrDefault(c => c.Type == ClaimNames.Role)?.Value ?? "None";
-            
-            foreach (var role in roles.Split(";"))
-            {
-                claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role));
-            }
-        }
+        if (!(user?.Identity?.IsAuthenticated ?? false)) return user ?? new ClaimsPrincipal();
         
-        return user ?? new ClaimsPrincipal();
+        var claimsIdentity = (ClaimsIdentity)user.Identity;
+        var claimRoles = claimsIdentity.Claims
+            .FirstOrDefault(c => c.Type == ClaimNames.Role, new Claim(ClaimNames.Role, "None"))
+            .Value;
+
+        AddRoles(claimsIdentity, claimRoles);
+
+        return user;
+    }
+    
+    private void AddRoles(ClaimsIdentity claimsIdentity, string claimRoles)
+    {
+        var roles = claimRoles.Split(';');
+        foreach (var role in roles)
+        {
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role));
+        }
     }
 }
