bc4190f7bdd2fe69073b12706ac7f2d1e6875612
Oscar Veldman <oveldman@gmail.com>
2023-06-03 21:00:51 +0200

    Add openApi security header

--- MadWorld/MadWorld.Backend.API.Authorized/Functions/CurriculumVitae/GetCurriculumVitae.cs
+++ MadWorld/MadWorld.Backend.API.Authorized/Functions/CurriculumVitae/GetCurriculumVitae.cs
@@ -1,12 +1,15 @@
 using System.Net;
 using LanguageExt;
 using MadWorld.Backend.API.Shared.Authorization;
+using MadWorld.Backend.API.Shared.OpenAPI;
 using MadWorld.Backend.Domain.CurriculaVitae;
 using MadWorld.Shared.Contracts.Anonymous.CurriculumVitae;
 using MadWorld.Shared.Contracts.Shared.Authorization;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.OpenApi.Models;
 
 namespace MadWorld.Backend.API.Authorized.Functions.CurriculumVitae;
 
@@ -21,6 +24,7 @@ public class GetCurriculumVitae
 
     [Authorize(RoleTypes.None)]
     [Function("GetCurriculumVitae")]
+    [OpenApiSecurity(Security.SchemeName, SecuritySchemeType.ApiKey, Name = Security.HeaderName, In = OpenApiSecurityLocationType.Header)]
     [OpenApiOperation(operationId: "GetCurriculumVitae", tags: new[] { "CurriculumVitae" })]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(GetCurriculumVitaeResponse), Description = "The OK response")] 
     public Option<GetCurriculumVitaeResponse> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "CurriculumVitae")] HttpRequestData request,
