d3b554819ccee3ac0b54f8a4c9a872772ebba7eb
Oscar Veldman <oveldman@gmail.com>
2023-08-01 14:43:16 +0200

    Finish GUID Generator

--- MadWorld/MadWorld.Frontend.UI.Suite/Pages/Tools/GuidGenerator.razor.cs
+++ MadWorld/MadWorld.Frontend.UI.Suite/Pages/Tools/GuidGenerator.razor.cs
@@ -4,11 +4,43 @@ namespace MadWorld.Frontend.UI.Suite.Pages.Tools;
 
 public partial class GuidGenerator
 {
-    private MonacoEditor _monacoEditor = default!;
+    private const int MinGuidAmount = 0;
+    private const int MaxGuidAmount = 10000;
     
-    private async Task GenerateGuid()
+    private MonacoEditor _monacoEditor = default!;
+
+    private int _guidAmountSelected = 1;
+    
+    private async Task GenerateGuids()
     {
-        var guid = Guid.NewGuid().ToString();
+        ValidateAmountOfGuids();
+        
+        var guid = GenerateGuidsToString();
         await _monacoEditor.SetValue(guid);
+    }
+
+    private string GenerateGuidsToString()
+    {
+        var guids = string.Empty;
+        
+        for (var i = 0; i < _guidAmountSelected; i++)
+        {
+            guids += Guid.NewGuid() + "\n";
+        }
+
+        return guids;
+    }
+
+    private void ValidateAmountOfGuids()
+    {
+        if (_guidAmountSelected < MinGuidAmount)
+        {
+            _guidAmountSelected = MinGuidAmount;
+        }
+
+        if (_guidAmountSelected > MaxGuidAmount)
+        {
+            _guidAmountSelected = MaxGuidAmount;
+        }
     }
 }
