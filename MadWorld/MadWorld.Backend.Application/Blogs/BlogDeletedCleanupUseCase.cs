64347f4ab0715f9347fb8123a0cb381add860e80
Oscar Veldman <oveldman@gmail.com>
2023-08-28 15:16:37 +0200

    Add some logging for jobrunner

--- MadWorld/MadWorld.Backend.Application/Blogs/BlogDeletedCleanupUseCase.cs
+++ MadWorld/MadWorld.Backend.Application/Blogs/BlogDeletedCleanupUseCase.cs
@@ -1,4 +1,5 @@
 using MadWorld.Backend.Domain.Blogs;
+using Microsoft.Extensions.Logging;
 
 namespace MadWorld.Backend.Application.Blogs;
 
@@ -6,21 +7,39 @@ public class BlogDeletedCleanupUseCase : IBlogDeletedCleanupUseCase
 {
     private readonly IBlogRepository _repository;
     private readonly IBlogStorageClient _storageClient;
+    private readonly ILogger<BlogDeletedCleanupUseCase> _logger;
 
-    public BlogDeletedCleanupUseCase(IBlogRepository repository, IBlogStorageClient storageClient)
+    public BlogDeletedCleanupUseCase(
+        IBlogRepository repository, 
+        IBlogStorageClient storageClient, 
+        ILogger<BlogDeletedCleanupUseCase> logger)
     {
         _repository = repository;
         _storageClient = storageClient;
+        _logger = logger;
     }
     
     public async Task StartCleanUp()
     {
         var blogs = _repository.GetDeletedBlogs();
-
+        
         foreach (var blog in blogs.Where(b => b.IsThirtyDaysOld()))
         {
-            await _storageClient.DeletePageAsync(blog.Id);
-            _repository.DeleteBlog(blog);
+            await PermanentDeleteBlog(blog);
         }
+    }
+    
+    private async Task PermanentDeleteBlog(Blog blog)
+    {
+        _logger.LogInformation("Start permanent delete blog {BlogId} with title {Title}",
+            blog.Id.ToString(),
+            blog.Title.ToString());
+        
+        await _storageClient.DeletePageAsync(blog.Id);
+        _repository.DeleteBlog(blog);
+        
+        _logger.LogInformation("Successfully blog deleted {BlogId} with title {Title}",
+            blog.Id.ToString(),
+            blog.Title.ToString());
     }
 }
